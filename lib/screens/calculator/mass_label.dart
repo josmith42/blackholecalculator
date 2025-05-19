7b655a687565310c903beab5b5b8e896e016e367
James O Smith <james.smith@willowtreeapps.com>
2025-05-19 17:27:54 -0400

    Add solar masses

--- lib/screens/calculator/mass_label.dart
+++ lib/screens/calculator/mass_label.dart
@@ -1,21 +1,31 @@
-
 import 'package:fling_units/fling_units.dart';
 
 enum MassLabel {
-    kilograms,
-    tonnes;
+  kilograms,
+  tonnes,
+  solarMasses;
 
   static MassLabel? from(Unit<Mass> unit) {
-    return MassLabel.values.firstWhere(
-      (label) => label.unit == unit,
-    );
+    return MassLabel.values.firstWhere((label) => label.unit == unit);
   }
 }
 
 extension MassLabelExtension on MassLabel {
   Unit<Mass> get unit => switch (this) {
-      MassLabel.kilograms => kilo.grams,
-      MassLabel.tonnes => tonnes,
+    MassLabel.kilograms => kilo.grams,
+    MassLabel.tonnes => tonnes,
+    MassLabel.solarMasses => solarMasses,
   };
   String get label => unit.toString();
 }
+
+class SolarMass extends Unit<Mass> {
+  const SolarMass()
+    : super(
+        name: 'M☉',
+        unitMultiplier: 1.9885e33,
+        prefix: const MeasurementPrefix.unit(),
+      );
+}
+
+const solarMasses = SolarMass();
