7fdf0f5a8e12a81265fde4687e1beee861d41205
James O Smith <james.smith@willowtreeapps.com>
2025-05-20 15:34:06 -0400

    Explanatory comment

--- lib/screens/calculator/unit_wrapper.dart
+++ lib/screens/calculator/unit_wrapper.dart
@@ -1,5 +1,7 @@
 import 'package:fling_units/fling_units.dart';
 
+// This class is used to wrap a unit and provide equality comparison for the DropdownMenu, as
+// the default equality check does not work as expected for the Unit class.
 class UnitWrapper<T extends Dimension> {
   final Unit<T> unit;
 
